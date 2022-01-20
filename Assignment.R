8d94831d53a97cf3034bc54675d941480ce52b95
Emily Carpenter <emily.carpenter@go.mnstate.edu>
2022-01-20 13:27:14 -0600

    second commit, rscript

--- Assignment.R
+++ Assignment.R
@@ -1,4 +1,32 @@
 library(tidyverse)
+library(palmerpenguins)
+
 penguins
-ggplot(data=penguins)+ geom_point(mapping = aes(x=body_mass_g, y= flipper_length_mm))
-ggplot(data=penguins)+ geom_point(mapping = aes(x=body_mass_g, y= flipper_length_mm, color=species))
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = body_mass_g, 
+                                                   y = flipper_length_mm))
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = body_mass_g, 
+                                                   y = flipper_length_mm, 
+                                                   color = species))
+
+ggplot(data = penguins) + geom_point(mapping = aes(
+  x = body_mass_g,
+  y = flipper_length_mm,
+  color = species,
+  shape = island
+))
+
+ggplot(data = penguins) + geom_point(mapping = aes(
+  x = body_mass_g,
+  y = flipper_length_mm,
+  color = species,
+  shape = island
+)) + facet_wrap( ~ species, ncol = 1)
+
+ggplot(data = penguins) + geom_point(mapping = aes(
+  x = body_mass_g,
+  y = flipper_length_mm,
+  color = species,
+  shape = island
+)) + facet_grid(species~ island)
